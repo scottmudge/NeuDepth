0279a79c2aab02ac7125c4a3a2af47ce474ed5f1
Scott Mudge <mail@scottmudge.com>
2023-05-17 02:57:00 -0400

    added ui

--- main.py
+++ main.py
@@ -4,14 +4,105 @@ from PIL import Image
 import os
 import sys
 from zoe.zoedepth.utils.misc import save_raw_16bit, colorize
-from time import time
+import time
 import glob
 import matplotlib.pyplot as plt
 import numpy as np
 import requests
 import json
+import dearpygui.dearpygui as dpg
+import shutil
+from threading import Thread
 
-PhoneCameraIP = "http://192.168.1.52:8080/shot.jpg"
+DefaultConfigPath = "./config/config.json"
+DefaultCameraIntrinsicsFileName = "camera_intrinsic.json"
+DefaultCameraIntrinsicsFilePath = "./config/%s"%DefaultCameraIntrinsicsFileName
+
+class Config:
+    DefaultConfigVars = {
+        'camera_ip': '192.168.1.54',
+        'camera_port': '8080',
+        'camera_img_path': '/shot.jpg'
+    }
+    
+    def __init__(self, path: str = DefaultConfigPath) -> None:
+        self.vars = dict()
+        self._path = path
+        self.load()
+        
+    def get(self, key: str):
+        if key in self.vars:
+            return self.vars[key]
+        elif key in Config.DefaultConfigVars:
+            return Config.DefaultConfigVars[key]
+        else:
+            raise KeyError("key not found in config: %s"%key)
+    
+    def set(self, key: str, value):
+        self.vars[key] = value
+        self.save()
+        
+    def load(self):
+        if os.path.exists(self._path):
+            with open(self._path, 'r') as file:
+                self.vars = json.load(file)
+    
+    def save(self):
+        with open(self._path, 'w') as file:
+            json.dump(self.vars, file, indent=4)    
+            
+class UI:
+    def __init__(self) -> None:
+        self.capture_sequence = False
+        self.capture_sequence_stopped = True
+    
+    def load_ui(self):
+        dpg.create_context()
+        dpg.create_viewport()
+        dpg.setup_dearpygui()
+        
+        with dpg.window(label="NeuDepth - Main", no_resize=True, no_collapse=True, no_close=True):
+            dpg.add_text("NeuDepth - Monocular 3D Reconstruction")
+            dpg.add_text("Note: Use IP Webcam app on Android")
+            dpg.add_input_text(label="Camera IP", default_value=config.get('camera_ip'), callback=self._ip_changed, decimal=True, no_spaces=True)
+            dpg.add_input_text(label="Camera Port", default_value=config.get('camera_port'), callback=self._port_changed, decimal=True, no_spaces=True)
+            dpg.add_button(label="Calibrate Camera", callback=self._calibrate_camera)
+            dpg.add_button(label="Capture Scan Sequence", callback=lambda: dpg.configure_item('scan_capture_window', show=True, pos=[200, 200]))
+            
+        with dpg.window(label="Scan Sequence Capture", tag="scan_capture_window", no_resize=True, no_close=True, no_collapse=True, show=False):
+            dpg.add_text("Press start to begin capture, be sure to start camera server")
+            dpg.add_input_text(label="Captured Frames", tag="captured_frames", readonly=True, default_value="0")
+            dpg.add_separator()
+            dpg.add_button(label="Start Capture", tag='start_capture_button', callback=self._capture_button)
+            
+        dpg.show_viewport()
+        dpg.start_dearpygui()
+        dpg.destroy_context()
+    
+    def _ip_changed(self, sender, app_data):
+        config.set('camera_ip', app_data)   
+    
+    def _port_changed(self, sender, app_data):
+        config.set('camera_port', app_data)
+    
+    def _calibrate_camera(self):
+        calibrate_camera()
+        
+    def _capture_button(self):
+        if not self.capture_sequence:
+            if not self.capture_sequence_stopped:
+                return
+            self.capture_sequence = True
+            dpg.configure_item('start_capture_button', label='Stop Capture')
+            thread = Thread(target=capture_image_sequence)
+            thread.start()
+        else:
+            self.capture_sequence = False
+            dpg.configure_item('start_capture_button', label='Start Capture')
+        
+ui = UI()
+
+config = Config()
 VideoResolution = (1024, 768)
 
 print("Welcome to Neudepth!")
@@ -35,9 +126,9 @@ def delete_folder_contents(folder: str):
 
 def estimate_depth(img_path: str, out_path: str, save: bool = False):
     img = Image.open(img_path).convert("RGB")
-    start_time = time()
+    start_time = time.time()
     depth_pil = zoe.infer_pil(img)
-    elapsed = time() - start_time
+    elapsed = time.time() - start_time
     print("\t> Depth estimation - elapsed: %.3f s"%elapsed)
     if save:
         if SaveRaw16Bit:
@@ -46,9 +137,9 @@ def estimate_depth(img_path: str, out_path: str, save: bool = False):
             Image.fromarray(colorize(depth_pil)).save(out_path)
     return depth_pil
 
-def estimate_depth_raw(img):
+def estimate_depth_raw(img, colorized: bool = False):
     depth = zoe.infer_pil(img)
-    return colorize(depth)
+    return colorize(depth) if colorized else depth
 
 def video_to_frames(video_path: str, frame_path: str, frame_rate: float = 0.5) -> list:
     """Returns list of paths to image frames"""
@@ -87,8 +178,9 @@ def video_to_frames(video_path: str, frame_path: str, frame_rate: float = 0.5) -
 
 def grab_frame_from_phone():
     resp = None
+    addr = "http://%s:%s%s"%(config.get('camera_ip'), config.get('camera_port'), config.get('camera_img_path'))
     try:
-        resp = requests.get(PhoneCameraIP, timeout=0.2)
+        resp = requests.get(addr, timeout=0.2)
         imgNp = np.array(bytearray(resp.content), dtype=np.uint8)
         if resp is not None: resp.close()
         return True, cv2.imdecode(imgNp, -1)
@@ -103,9 +195,54 @@ def compute_depth_from_camera():
             break
     cv2.destroyAllWindows()
     sys.exit(0)
+
+def capture_image_sequence():
+    if not os.path.exists(DefaultCameraIntrinsicsFilePath):
+        print("Cannot capture scan sequence! Please calibrate your camera first...")
+        return
     
+    ui.capture_sequence_stopped = False
     
-def calibrate_camera(out_calib_file: str = "./out/camera_intrinsic.json"):
+    target_dir = "./scan_sequences/%d"%int(time.time())
+    color_dir = "%s/color"%target_dir
+    depth_dir = "%s/depth"%target_dir
+    
+    os.makedirs(target_dir, exist_ok=True)
+    os.makedirs(color_dir, exist_ok=True)
+    os.makedirs(depth_dir, exist_ok=True)
+    
+    shutil.copyfile(DefaultCameraIntrinsicsFilePath, "%s/%s"%(target_dir, DefaultCameraIntrinsicsFileName))    
+    print("Starting scan sequence capture:\n\t> Dir: %s"%target_dir)
+    
+    cur_idx = 0
+    
+    while ui.capture_sequence:
+        if (cv2.waitKey(1) == 27):
+            ui.capture_sequence = False
+            dpg.configure_item('start_capture_button', label='Start Capture')
+            continue
+        
+        res, image = grab_frame_from_phone()
+        if not res: continue
+        depth = estimate_depth_raw(image)
+        
+        cv2.imshow("Color Image", image)
+        cv2.imshow("Depth", colorize(depth))
+        
+        depth_filename = "%s/%s.png"%(depth_dir, str(cur_idx).zfill(6))
+        rgb_filename = "%s/%s.jpg"%(color_dir, str(cur_idx).zfill(6))
+        
+        save_raw_16bit(depth, depth_filename)
+        cv2.imwrite(rgb_filename, image, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
+                
+        cur_idx += 1    
+        
+    print("Scan sequence capture ended...")
+    cv2.destroyAllWindows()        
+    ui.capture_sequence_stopped = True
+        
+    
+def calibrate_camera(out_calib_file: str = DefaultCameraIntrinsicsFilePath):
     CHECKERBOARD = (6, 9)
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
     threedpoints = []
@@ -188,22 +325,5 @@ def calibrate_camera(out_calib_file: str = "./out/camera_intrinsic.json"):
             
         print("calibration written to %s"%out_calib_file)
 
-if __name__ == "__main__":
-    
-    calibrate_camera()
-    sys.exit(0)
-
-    
-    if len(sys.argv) < 2:
-        print("Not enough arguments")
-        sys.exit(-1)
-    
-    input_file = sys.argv[1]
-    if not os.path.exists(input_file):
-        print("Input file not found: %s"%input_file)
-        
-    # Is video? Convert to frames
-    if ".mp4" in input_file:
-        video_to_frames(input_file, VideoTempPath)
-    #estimate_depth(load_image(sys.argv[1]))
-        
+if __name__ == "__main__":    
+    ui.load_ui()
